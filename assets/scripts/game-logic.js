262f01e4126e3ac368753d4a7c7cf250af7c269b
mfbrewster <michael.f.brewster@gmail.com>
2016-02-03 15:51:21 -0500

    added helpful comments

--- assets/scripts/game-logic.js
+++ assets/scripts/game-logic.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// All variable stored in a single object
 let gameState = {
   player: '',
   over: false,
@@ -18,6 +19,7 @@ let gameState = {
       },
   },
 
+  // Method to toggle player
   changePlayer: function() {
     if (!this.player || this.player === 'o') { this.player = 'x';
     } else { this.player = 'o'; }
@@ -52,90 +54,116 @@ let linesForSquare = [
   ['bottom', 'right', 'diagDown'],            // 8
 ];
 
-// sets all values of the board tray to null
+// sets all values of the board tray to null...
 let clearBoard = function(board) {
   for (let i = 0; i < 9; i++) {
     board[i] = '';
   }
+  // ... and clears the HTML board
   $('.game-box').children().html("");
   return;
 };
 
+// Updates the score box displays
 let displayScore = function(score) {
   $('#x .score').html(score.x);
   $('#o .score').html(score.o);
   $('#tie .score').html(score.tie);
-}
+};
 
 // checks if a win has occurred on a given line
 let winCheck = function(coordArray, board) {
   let boxVals = [];
 
-  // concatenates values at given board coordinates
+  // Creates an array of values located at coordinate positions
   coordArray.forEach(function(val, index) {
     boxVals.push(board[(coordArray[index][1] * 3) + coordArray[index][0]]);
   });
 
-  // returns true if a line has been completed by either player
-  // !! WILL PROBABLY NOT NEED TO CHECK FOR BOTH PLAYERS !!
+  // returns true if all values stored in the array are equivalent
   if (boxVals[0] === boxVals[1] && boxVals[0] === boxVals[2]) { return true; }
   else { return false; }
+};
+
+// Adds point to score of current player if a player won; otherwise adds a point
+// to the score for "tie". Increments gamecounter, sets "gameState.over" to
+// true, and updates the score display
+let endGame = function(gameState, playerWin) {
+  if (playerWin) {
+    gameState.score[gameState.player]++;
+  } else {
+    gameState.score.tie++;
+  }
+  gameState.game++;
+  gameState.over = true;
+  displayScore(gameState.score);
+  return;
 };
 
 // clears the board, sets the player based on the game count, resets the turn
 // count,and increments the game count
 let newGame = function(gameState, board) {
   clearBoard(board);
-  gameState.over = false;
   displayScore(gameState.score);
-  // $('#x .score').html(gameState.score.x);
-  // $('#o .score').html(gameState.score.o);
-  // $('#tie .score').html(gameState.score.tie);
-  console.log(gameState.score);
-
   if (gameState.game % 2 === 0) { gameState.player = 'x'; }
   else { gameState.player = 'o'; }
-
+  gameState.over = false;
   gameState.turn = 0;
-  gameState.game++;
   return;
 };
 
-// Sets value of a square, then checks to see whether the move results in a win
+// Sets value of a square, then checks to see whether the move results in a win.
+// If a player makes a winning move, or nine turns have passed without a win,
+// the game ends.
 let setSquare = function(index, gameState, board) {
   board[index] = gameState.player;
-  console.log(board);
                     // "linesForSquare[index]" is an array of strings, which
                     // corrrespond to keys in the "lines" object
   for (let i = 0; i < linesForSquare[index].length; i++) {
                 // for each key in "linesForSquare[index]", checks coordinates
                 //  stored in "lines" object to see if the game is won
     if (winCheck( lines[linesForSquare[index][i]], board) ) {
-      gameState.score[gameState.player]++;
-      newGame(gameState, board);
+      endGame(gameState, true);
       return true;
     }
   }
+
+  // If statement which either increments the turn
+  // counter, or ends the game in a tie
   if (gameState.turn < 8) {
     gameState.turn++;
   } else {
-    gameState.score.tie++;
-    newGame(gameState, board);
+    endGame(gameState, false);
   }
+
+  // if the game doesn't end, toggles player
   gameState.changePlayer();
   return false;
 };
 
+// On page load, sets up the board and sets event listeners
 $(document).ready(() => {
   newGame(gameState, board);
+
+  // If any of the squares on the game board are clicked...
   $('.game-box').children().on('click', function() {
+    // "move" is set to a value on the board from 0-8
     let move = event.target.id;
-    $(this).html(gameState.player);
-    if (!board[move]/* && !gameState.over*/) {
+    // if the position on the board is empty, and the gameState.over variable
+    // is not set to true, the board display indicated the move and the game
+    // setSquare variable checks the win conditions
+    if (!board[move] && !gameState.over) {
+      $(this).html(gameState.player);
       gameState.over = setSquare(move, gameState, board);
     }
   });
-  $('#new-game').on('click', function() {
 
+  // When the new-game button is clicked, the player is toggled before the
+  // newGame function is called. This is so, if the button is pressed before
+  // the game ends and the game counter is incremented, the same player
+  // who started the current game will start the next one
+  $('#new-game').on('click', function() {
+    gameState.changePlayer();
+    newGame(gameState, board);
   });
 });
